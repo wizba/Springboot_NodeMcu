657ca8b5160145cab749c3e0aa1ccf6843d2f819
william Rabopape <williamrabs@gmail.com>
2019-10-16 16:14:26 +0200

    iot 1

---
+++ IOT/src/main/java/com/example/IOT/IotApplication.java
@@ -0,0 +1,13 @@
+package com.example.IOT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IotApplication.class, args);
+	}
+
+}
